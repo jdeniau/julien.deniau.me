909e76d118e33a585a91e0761d6fb2fe51a713b0
Julien Deniau <julien.deniau@gmail.com>
2025-06-02 21:34:43 +0000

    Reveal as node_module dependency

---
+++ conf/reveal-bootstrap/index.ts
@@ -0,0 +1,10 @@
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import 'reveal.js/dist/reveal.css';
+import 'reveal.js/dist/theme/black.css';
+
+let deck = new Reveal({
+  plugins: [Markdown],
+  showNotes: true,
+});
+deck.initialize();
