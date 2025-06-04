03e9967efee4e57182c901e55543512ecb278d48
Julien Deniau <julien.deniau@gmail.com>
2025-06-04 13:44:37 +0000

    migrate to new reveal unified system

---
+++ talk/voyager-autrement/index.ts
@@ -0,0 +1,7 @@
+import 'reveal.js/dist/reveal.css';
+import 'reveal.js/dist/theme/beige.css';
+import 'reveal.js/plugin/highlight/monokai.css';
+
+import deck from '../RevealConfig';
+
+deck.initialize();
