12f7e03ac75f4a661a0364e3c6931ea8df0317cd
Julien Deniau <julien.deniau@gmail.com>
2025-06-04 12:58:44 +0000

    centralized default for all talks

--- talk/reveal-bootstrap/index.ts
+++ talk/reveal-bootstrap/index.ts
@@ -1,43 +1,6 @@
-import Reveal from 'reveal.js';
-import Highlight from 'reveal.js/plugin/highlight/highlight';
-import RevealMarkdown from 'reveal.js/plugin/markdown/markdown';
-import RevealNotes from 'reveal.js/plugin/notes/notes';
-
 import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/black.css';
 import 'reveal.js/plugin/highlight/monokai.css';
+import deck from '../RevealConfig';
 
-let deck = new Reveal({
-  // enable hash navigation
-  hash: true,
-
-  // do not display a presentation progress bar
-  progress: false,
-
-  // display slide numbers, but only for the speaker
-  slideNumber: true,
-  showSlideNumber: 'speaker',
-
-  // activate controls only for the speaker
-  // @ts-expect-error -- until https://github.com/DefinitelyTyped/DefinitelyTyped/pull/72951 is merged
-  controls: 'speaker-only',
-
-  // Global override for autolaying embedded media (video/audio/iframe)
-  // - null:   Media will only autoplay if data-autoplay is present
-  // - true:   All media will autoplay, regardless of individual setting
-  // - false:  No media will autoplay, regardless of individual setting
-  autoPlayMedia: null,
-
-  // activate plugins
-  plugins: [
-    // syntax highlighting
-    Highlight,
-    // markdown support
-    RevealMarkdown,
-    // speaker notes
-    RevealNotes,
-  ],
-  // enable notes on build to publish to the web
-  showNotes: process.env.NODE_ENV === 'production',
-});
 deck.initialize();
