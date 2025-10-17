fb5ab07e0f16f8812b42d93f62b9856e05ceba98
Julien Deniau <julien.deniau@gmail.com>
2025-10-17 14:58:29 +0200

    Voyager autrement : commencement vélo

--- talk/voyager-autrement/index.ts
+++ talk/voyager-autrement/index.ts
@@ -1,6 +1,7 @@
 import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/beige.css';
 import 'reveal.js/plugin/highlight/monokai.css';
+import './css/voyager-autrement.css';
 
 import deck from '../RevealConfig';
 
