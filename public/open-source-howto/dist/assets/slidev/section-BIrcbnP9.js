f718f8fed61c5dac4455156c8ca0f90fca2c9705
Julien Deniau <julien.deniau@gmail.com>
2026-02-12 12:52:18 +0100

    build

---
+++ public/open-source-howto/dist/assets/slidev/section-BIrcbnP9.js
@@ -0,0 +1,1 @@
+import{o as t,b as s,e as o,f as a}from"../modules/vue-BbQqWA2w.js";import{u as r}from"./context-D5YmNz-x.js";const c={class:"slidev-layout section w-full h-full grid"},l={class:"my-auto text-center"},u={__name:"section",setup(n){return r(),(e,i)=>(t(),s("div",c,[o("div",l,[a(e.$slots,"default")])]))}};export{u as _};
